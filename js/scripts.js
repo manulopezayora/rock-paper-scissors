b830bd8b4465a1756eb8d5946bda5b096218f970
ManuelJesusLopez <manulopezayora1985@protonmail.com>
2020-08-12 17:37:01 +0200

    Timer Play & round bucle

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,41 @@ const rules = document.getElementById("rules")
 
 // * GLOBALS
 let userIsActive = false
+let selectedRounds = 0
+let humanScore = 0
+let cpuScore = 0
 const tokens = [{ name: "paper" }, { name: "scissors" }, { name: "rock" }]
+const result = [
+  [0, 1, 2],
+  [2, 0, 1],
+  [1, 2, 0],
+]
 
 // * FUNCTIONS
 
 const renderApp = () => {
   // ! cambiar a true!!!
-  if (userIsActive === false) printGame()
+  if (userIsActive === true) printGame()
   else form.classList.remove("hidden")
 }
 
-// Add Rounds to session storage
 const setRounds = (rounds) => {
-  sessionStorage.setItem("rounds", JSON.stringify(rounds))
+  selectedRounds = rounds
   userIsActive = true
+}
+
+const timer = () => {
+  const countdownElement = document.getElementById("countdown")
+  let selectTime = 5
+  const interval = setInterval(() => {
+    selectTime--
+    if (selectTime < 1) {
+      clearInterval(interval)
+      countdownElement.innerHTML = "Finished time"
+    }
+    const time = `Chose you're play: ${selectTime} seconds`
+    countdownElement.innerHTML = time
+  }, 1000)
 }
 
 const printGame = () => {
@@ -28,26 +49,33 @@ const printGame = () => {
   const table = document.createElement("SECTION")
   table.classList.add("table")
 
+  const countdown = document.createElement("span")
+  countdown.classList.add("countdown")
+  countdown.setAttribute("id", "countdown")
+  countdown.textContent = "Chose you're play: 5 seconds"
+
   tokens.map((token) => {
     const tokenElement = document.createElement("DIV")
-    tokenElement.classList.add("token", `${token.name}`)
+    tokenElement.classList.add("token", token.name)
+    tokenElement.setAttribute("data-value", token.name)
     tokenElement.innerHTML = `
       <div class="token-outline-top">
-      <img src="../images/icon-${token.name}.svg" alt="icon for play" data-value="${token.name}" />
+      <img src="../images/icon-${token.name}.svg" alt="icon for play" />
       </div>
     `
-
     table.append(tokenElement)
   })
-
-  fragment.append(table)
-
+  fragment.append(countdown, table)
   app.append(fragment)
+  timer()
 }
 
-const playGame = () => {
-  // Traer el valor del session storage
-  // const token = JSON.parse(sessionStorage.getItem("token"))
+const playRound = () => {
+  let currentRound = 0
+  while (currentRound < selectedRounds) {
+    currentRound++
+    console.log(selectedRounds, currentRound)
+  }
 }
 
 // * EVENTS
@@ -65,12 +93,13 @@ form.addEventListener("submit", (e) => {
   const rounds = parseInt(e.target[0].value)
   setRounds(rounds)
   renderApp()
-  playGame()
+  playRound()
 })
 
 app.addEventListener("click", (e) => {
   const target = e.target.dataset.value
-  console.log(target)
+  if (target === "rock" || target === "paper" || target === "scissors")
+    console.log(target)
 })
 
 window.addEventListener("load", renderApp())
